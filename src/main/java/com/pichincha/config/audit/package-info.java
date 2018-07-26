d71e6379810405252ea3161aa48262199869a8c2
agamgupta6 <agamgupta1988@gmail.com>
2018-07-26 14:39:56 -0500

    Initial application generated by JHipster-4.14.3

---
+++ src/main/java/com/pichincha/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.pichincha.config.audit;
---
+++ src/main/java/com/pichincha/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.pichincha.repository;
+
+import com.pichincha.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the Authority entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
